5cb41207d99aa559fb9063710b0a2cbdcee062f0
lostzilla <alanaa92@gmail.com>
2023-01-09 22:40:58 -0300

    poc: first java code

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+      //number to be multiplied
+      int value = 5;
+
+      System.out.println("Tabela de multiplicação de " + value);
+       for (int i = 1; i <= 10; i++) {
+
+        System.out.println(value + " X " + i + " = " + value * i);
+        
+       }
+    }
+}
